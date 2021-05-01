7c64fcd43e30656c48fcdb565af81b655223cdfa
eshaant7 <68991885+eshaant7@users.noreply.github.com>
2021-05-01 11:31:45 +0200

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,34 @@
+#  MADE BY:_            _   _____                        _
+#  \ \    / (_)        (_) |  __ \                      (_)
+#   \ \  / / _ _ __ ___ _  | |  | | __ _ ___  __ _ _ __  _
+#    \ \/ / | | '__/ _ \ | | |  | |/ _` / __|/ _` | '_ \| |
+#     \  /  | | | |  __/ | | |__| | (_| \__ \ (_| | | | | |
+#      \/   |_|_|  \___| | |_____/ \__,_|___/\__,_|_| |_|_|
+#                     _/ |
+#                    |__/
+# A Number Guessing Game
+
+from random import randint
+
+answer = randint(1, 9)
+
+user_input = int(input("Guess any number between 0 and 10\n"))
+
+numberOfGuesses = 2
+
+while numberOfGuesses > 0:
+    if user_input == answer:
+        print("CORRECT")
+        user_input = int(input(""))
+
+    elif user_input > answer:
+        print("The number is smaller, Guess Again")
+        user_input = int(input(""))
+        numberOfGuesses -= 1
+
+    elif user_input < answer:
+        print("The number is bigger, Guess Again")
+        user_input = int(input("")) 
+        numberOfGuesses -= 1
+       
+          
